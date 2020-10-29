184d395187e280b85d2a62994eb9fd66738de86d
Annie Mao <anniemao@microsoft.com>
2020-10-29 13:38:01 -0700

    edit chmod

--- runPerftool.psm1
+++ runPerftool.psm1
@@ -40,7 +40,7 @@ Param ([Int]$AdditionalTimeout, [string] $Line)
 
 $ScriptBlockEnableToolPermissions = {
     param ($remoteToolPath, $creds)
-    Write-Output $creds.GetNetworkCredential().Password | sudo -S chmod 777 $remoteToolPath
+    sudo chmod 777 $remoteToolPath
 } # $ScriptBlockEnableToolPermissions()
 
 $ScriptBlockCleanupFirewallRules = {
@@ -75,7 +75,7 @@ $ScriptBlockCreateDirForResults = {
 
         if (!(Test-Path $folderToCreate)) {
             New-Item -Force -ItemType Directory -Path $folderToCreate
-            Write-Output $creds.GetNetworkCredential().Password | sudo -S chmod 777 $folderToCreate
+            sudo chmod 777 $folderToCreate
         }   
 
     }
